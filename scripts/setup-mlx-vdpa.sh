9a34a149919b3651a08ce77f7ec45ab5702bc1b4
Adrian Moreno <amorenoz@redhat.com>
2021-02-09 12:21:41 +0100

    scripts: bind devices to vhost and virtio drivers
    Bind half of the devices to vhost and the other half to virtio vdpa
    driver.
    
    Signed-off-by: Adrian Moreno <amorenoz@redhat.com>

--- scripts/setup-mlx-vdpa.sh
+++ scripts/setup-mlx-vdpa.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 set -eu
  
-usage() {
+function usage() {
         echo "$0 PCIADDR [NUM_VFs]"
         echo "  PCIADDR: The PCI address of the PF, e.g: 0000:40:00.0"
         echo "  NUM_VFS (defaul = 4): Number of VFs to configure"
         exit 1
 }
 
-error() {
+function error() {
         echo $@
         exit 1
 }
 
-get_pci_addr() {
+function get_pci_addr() {
         pf=$1
         vf=$2
         if [ -z $vf ]; then
@@ -21,6 +21,22 @@ get_pci_addr() {
         else
                 echo $(basename $(readlink /sys/class/net/${pf}/device/virtfn${vf}))
         fi
+}
+
+# $1 is the vdpa device
+# $2 is the desired vdpa driver
+function set_vdpa_driver() {
+        local dev=$1
+        local driver=$2
+            if [ -d "/sys/bus/vdpa/devices/${dev}/driver" ]; then
+                    local curr_driver=$(basename $(readlink /sys/bus/vdpa/devices/${dev}/driver))
+                    if [[ "${curr_driver}" != "${driver}" ]]; then
+                        echo "${dev}" > /sys/bus/vdpa/drivers/${curr_driver}/unbind
+                else 
+                        return 0
+                fi
+        fi
+        echo "${dev}" > /sys/bus/vdpa/drivers/${driver}/bind
 }
 
 if [ "$EUID" -ne 0 ]
@@ -48,9 +64,10 @@ for i in $(seq 0 $(($num_vfs -1))); do
         echo $pci_addr >  /sys/bus/pci/drivers/mlx5_core/unbind
 done
 
-echo "Modprobbing mlx5_vdpa"
+echo "Modprobbing vdpa drivers"
 modprobe vdpa || true
 modprobe vhost-vdpa || true
+modprobe virtio-vdpa || true
 modprobe mlx5_vdpa || true
 
 echo "Binding mlx5_core"
@@ -66,16 +83,13 @@ done
 modprobe vhost_vdpa || true
 
 echo ""
-echo "Binding devices to vhost-vdpa driver"
-for dev in $(ls -x /sys/bus/vdpa/devices); do
-    if [ -d "/sys/bus/vdpa/devices/$dev/driver" ]; then
-    	driver=$(basename $(readlink /sys/bus/vdpa/devices/$dev/driver))
-    	if [[ "$driver" != "vhost_vdpa" ]]; then
-        	echo $dev > /sys/bus/vdpa/drivers/$driver/unbind
-	else
-		continue
-	fi
-    fi  
-    echo $dev > /sys/bus/vdpa/drivers/vhost_vdpa/bind
+echo "Binding half of the devices to vhost-vdpa and other half to virtio-vpda"
+DRIVERS=("vhost_vdpa" "virtio_vdpa")
+devices=( $(ls -x /sys/bus/vdpa/devices) )
+for i in ${!devices[@]}; do
+    dev=${devices[$i]}
+    driver=${DRIVERS[$(($i%2))]}
+    echo "Binding device ${dev} to driver ${driver}"
+    set_vdpa_driver $dev $driver
 done
 
